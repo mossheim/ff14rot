64e733161f025d926239fa47e4cdcf08e60ff316
Brian Heim <self@brianlheim.com>
2021-11-10 13:37:27 +0100

    don't use fp arithmetic

--- main.cpp
+++ main.cpp
@@ -286,7 +286,7 @@ Rotation pruningOptimalRotation(const Job& job, Time duration, Time gcdDelay, in
             for (auto& action : job.actions) {
                 // Put all time-viable candidates in pq2 ensuring it doesn't grow too large
                 auto time = action.startTime(rot, gcdDelay);
-                if (time - state.time() < gcdDelay * 1.1 && time < duration) {
+                if (time - state.time() < gcdDelay + 1 && time < duration) {
                     auto newState = state;
                     newState.advanceTo(time);
                     newState.processAction(action);
